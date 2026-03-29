c35ef191eb9ea49621145705bcb080648981129d
Ruzgar5 <mruzgarcelik@gmail.com>
2026-03-29 16:00:56 +0300

    New Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -14,27 +14,25 @@ import edu.wpi.first.math.Matrix;
 
 public final class Constants {
     public static final class FuelConstants {
-        public static final int LEFT_INTAKE_LAUNCHER_MOTOR_ID = 12;
-        public static final int RIGHT_INTAKE_LAUNCHER_MOTOR_ID = 2;
-        public static final int INDEXER_MOTOR_ID = 13;
+        public static final int LEFT_INTAKE_LAUNCHER_MOTOR_ID = 5;//12
+        public static final int RIGHT_INTAKE_LAUNCHER_MOTOR_ID = 15;//2
+        public static final int INDEXER_MOTOR_ID = 16;//13
         public static final int INDEXER_MOTOR_CURRENT_LIMIT = 80;
         public static final int LAUNCHER_MOTOR_CURRENT_LIMIT = 80;
-
-        public static final double INDEXER_INTAKING_PERCENT = -0.2;
-        public static final double INDEXER_LAUNCHING_PERCENT = 0.2;
-        public static final double INDEXER_SPIN_UP_PRE_LAUNCH_PERCENT = -0.2;
-        public static final double INTAKE_INTAKING_PERCENT = 0.2;
-        public static final double LAUNCHING_LAUNCHER_PERCENT = 0.2;
-        public static final double INTAKE_EJECT_PERCENT = -0.2;
-
+        public static final double INDEXER_INTAKING_PERCENT = 0.8;
+        public static final double INDEXER_LAUNCHING_PERCENT = -0.6;
+        public static final double INDEXER_SPIN_UP_PRE_LAUNCH_PERCENT = -0.5;
+        public static final double INTAKE_INTAKING_PERCENT = 0.6;
+        public static final double LAUNCHING_LAUNCHER_PERCENT = 0.85;
+        public static final double INTAKE_EJECT_PERCENT = -0.8;
         public static final double SPIN_UP_SECONDS = 0.75;
     }
 
     public static final class ClimbConstants {
-        public static final int CLIMBER_MOTOR_ID = 3;
+        public static final int CLIMBER_MOTOR_ID = 4;//3
         public static final int CLIMBER_MOTOR_CURRENT_LIMIT = 40;
-        public static final double CLIMBER_MOTOR_DOWN_PERCENT = -0.2;
-        public static final double CLIMBER_MOTOR_UP_PERCENT = 0.2;
+        public static final double CLIMBER_MOTOR_DOWN_PERCENT = -0.8;
+        public static final double CLIMBER_MOTOR_UP_PERCENT = 0.8;
     }
 
     public static final class OperatorConstants {
